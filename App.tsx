396b3b517d3140aa37cc47ff37e545892af61551
caioteca <caiodev2020@gmail.com>
2023-08-29 00:56:13 +0100

    :tada: primeiro commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts,
+} from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import { Signin } from './src/screens/Signin';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+          setAppIsReady(true);
+        }
+      } catch (error) {
+        console.warn('Error while preparing the app:', error);
+      }
+    };
+
+    prepareApp();
+  }, [fontsLoaded]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar backgroundColor="transparent" translucent />
+      <Signin />
+    </>
+  );
+}
